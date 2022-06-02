86f71d46b6221a557103eeb38d7ab30617816989
dekapro9x <dekapro9x@gmail.com>
2022-06-02 23:07:40 +0700

    Update socket.io hooking express.js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
+//Móc socket.io vào press.js:
 const express = require("express");
-const server = express();
-const routes = require('./src/route');
 const cors = require("cors");
-server.use(express.json());
-server.use(cors());
-server.use(routes);
-server.listen(3000, function(){
-    console.log('Connected Successfull!');
+const app = express();
+const routes = require('./src/route');
+const port = 3000;
+//Express gắn thêm route:
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+const server = app.listen(port, () => {
+    console.log("Listening on port: " + port);
 });
+
+const socketIO = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+socketIO.on("connection", (socket) => {
+    console.log("Đã có 1 kết nối:");
+    //Lắng nghe kết nối khởi tạo - báo lại cho client nếu kết nối thành công!
+    socket.on("check-connect-to-server", (event) => {
+        if (event && event == "client-connect-to-server") {
+            console.log("Đã nghe thấy 1 event:", event);
+            socket.emit("connect-to-server", "CONNECT-TO-SERVER-SUCCESS");
+        }
+    })
+
+    //Lắng nghe tin nhắn gửi đi từ client:
+    socket.on("client_sent_mess_to_server", function (mess) {
+        console.log("Đã nghe thấy 1 tin nhắn:", mess);
+        const idMessRandom = `${Math.random()}`;
+        console.log(idMessRandom);
+        socket.emit("server-rep", [
+            {
+                text: 'Hãy nói gì đi!...',
+                user: { _id: 2 },
+                createdAt: '2022-05-31T16:16:57.048Z',
+                _id: idMessRandom
+            }
+        ])
+    })
+
+    //Ngắt kết nối client - server khi 1 bên rời khỏi:
+    socket.on("disconnect", () => { });
+});
+
 
