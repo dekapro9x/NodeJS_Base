d8493d6fe2c6462fd5d4651482ffeb98a0c7af82
dekapro9x <dekapro9x@gmail.com>
2022-06-02 22:46:06 +0700

    Update server login API

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Bước 1: Import module http
-const http = require('http');
-// Bước 2: Khởi tạo server
-const server = http.createServer(function(request, response){
-    response.writeHead(200, {
-        "Context-type" : "text/plain"
-    });
-    response.write('Your URL is ' + request.url);
-    response.end();
-});
- 
-// Bước 3: Lắng nghe cổng 300 thì thực hiện chương trình
+const express = require("express");
+const server = express();
+const routes = require('./src/route');
+const cors = require("cors");
+server.use(express.json());
+server.use(cors());
+server.use(routes);
 server.listen(3000, function(){
     console.log('Connected Successfull!');
 });
+
